71556d18c67c255db7cc3ed8c67712e66ae685b5
Viktor Skyba <skyba.viktor2016@gmail.com>
2025-10-04 16:39:02 +0300

    feat: add for loop

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log('Hello World!');
+
+for (let index = 1; index <= 5; index++) {
+    if (index == 3) {
+        console.log("Function found 3.");
+        index = 5;
+    } else {
+        console.log(index);
+    }
+    
+}
