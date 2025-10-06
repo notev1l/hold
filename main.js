2814757a501e9023f88f2d5c579221a831a68e74
Viktor Skyba <skyba.viktor2016@gmail.com>
2025-10-06 16:08:14 +0300

    feat: math test

--- main.js
+++ main.js
@@ -8,4 +8,14 @@ for (let index = 1; index <= 5; index++) {
         console.log(index);
     }
     console.log('test drive');
+    console.log('Result:', (3 + -3) / 2);
+    x1 = 0;
+    y1 = -1;
+    x2 = 1;
+    y2 = 1;
+    console.log(Math.cos((x1 * x2 + y1 * y2) / Math.sqrt(x1 ** 2 + y1 ** 2)) *
+    Math.sqrt(x2 ** 2 + y2 ** 2));
+    console.log(((x1 * x2 + y1 * y2)));
+    console.log(Math.sqrt(x1 ** 2 + y1 ** 2));
+    console.log(Math.sqrt(x2 ** 2 + y2 ** 2));
 }
